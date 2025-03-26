e47d8aa94c02798e15ece86f0b932aba20c4b6f6
DevVc <103268689+DevVc7@users.noreply.github.com>
2025-03-25 23:55:51 -0500

    Fix Front

--- src/LabCamaronWeb.Infraestructura/Constantes/Procesos/ConstantesTipoEntidad.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LabCamaronWeb.Infraestructura.Constantes.Procesos
-{
-    public static class ConstantesTipoEntidad
-    {
-        public const string Natural = "NAT";
-        public const string Juridica = "JUR";
-    }
-}
